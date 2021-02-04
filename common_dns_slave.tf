a568bacd4377d280e9f488fe0e696399d68a75f8
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-02-04 05:06:24 +0000

    hosted zone
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- common_dns_slave.tf
+++ common_dns_slave.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "monitoring_loadbalancer_slave" {
   count    = local.is_management_env ? 0 : 1
   name     = join(".", [local.roles[local.secondary_role_index], local.fqdn])
   type     = "A"
-  zone_id  = aws_service_discovery_private_dns_namespace.monitoring.hosted_zone
+  zone_id  = aws_service_discovery_private_dns_namespace.monitoring_slave.hosted_zone
 
   alias {
     evaluate_target_health = false
@@ -17,7 +17,7 @@ resource "aws_route53_record" "prometheus_loadbalancer" {
   count    = local.is_management_env ? 0 : 1
   name     = "prometheus.${local.fqdn}"
   type     = "A"
-  zone_id  = aws_service_discovery_private_dns_namespace.monitoring.hosted_zone
+  zone_id  = aws_service_discovery_private_dns_namespace.monitoring_slave.hosted_zone
 
   alias {
     evaluate_target_health = false
@@ -44,7 +44,7 @@ resource "aws_route53_record" "monitoring_slave" {
   count           = local.is_management_env ? 0 : 1
   name            = tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[0].resource_record_name
   type            = tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[0].resource_record_type
-  zone_id         = aws_service_discovery_private_dns_namespace.monitoring.hosted_zone
+  zone_id         = aws_service_discovery_private_dns_namespace.monitoring_slave.hosted_zone
   records         = [tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[0].resource_record_value]
   ttl             = 60
   allow_overwrite = true
@@ -56,7 +56,7 @@ resource "aws_route53_record" "prometheus" {
   count           = local.is_management_env ? 0 : 1
   name            = tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[1].resource_record_name
   type            = tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[1].resource_record_type
-  zone_id         = aws_service_discovery_private_dns_namespace.monitoring.hosted_zone
+  zone_id         = aws_service_discovery_private_dns_namespace.monitoring_slave.hosted_zone
   records         = [tolist(aws_acm_certificate.monitoring_slave[local.secondary_role_index].domain_validation_options)[1].resource_record_value]
   ttl             = 60
   allow_overwrite = true
@@ -70,4 +70,29 @@ resource "aws_acm_certificate_validation" "monitoring_slave" {
     aws_route53_record.monitoring_slave[local.secondary_role_index].fqdn,
     aws_route53_record.prometheus[local.secondary_role_index].fqdn
   ]
+}
+
+resource "aws_route53_zone" "monitoring_slave" {
+  name = "${local.environment}.services.${var.parent_domain_name}"
+  vpc {
+    vpc_id = module.vpc.outputs.vpcs[0].id
+  }
+  tags = merge(local.tags, { Name = "${var.name}-${var.secondary}" })
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+}
+
+resource "aws_route53_vpc_association_authorization" "monitoring_slave" {
+  count   = local.is_management_env ? 0 : 1
+  vpc_id  = local.is_management_env ? module.vpc.outputs.vpcs[0].id : data.terraform_remote_state.management_dmi.outputs.vpcs[0].id
+  zone_id = aws_service_discovery_private_dns_namespace.monitoring_slave.hosted_zone
+}
+
+resource "aws_route53_zone_association" "monitoring_slave" {
+  for_each   = local.is_management_env ? local.dns_zone_ids[local.environment] : {}
+  provider   = aws.management_zone
+  vpc_id     = module.vpc.outputs.vpcs[0].id
+  zone_id    = each.value
+  depends_on = [aws_route53_vpc_association_authorization.monitoring_slave]
 }
